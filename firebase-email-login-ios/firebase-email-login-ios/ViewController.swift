0b8ca10d66c160b78f20ff2d988bf6cc1cbc8996
yoky-ja <yoky.ja@gmail.com>
2017-03-22 16:37:22 +0700

    Complete 1

--- firebase-email-login-ios/firebase-email-login-ios/ViewController.swift
+++ firebase-email-login-ios/firebase-email-login-ios/ViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var EmailField: UITextField!
+    @IBOutlet weak var PassField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func LoginBtnPressed(_ sender: Any) {
+        Login()
+    }
+    
+    func Login(){
+        FIRAuth.auth()?.signIn(withEmail: EmailField.text!, password: PassField.text!, completion: { (user, error) in
+            if error != nil{
+                print("Login Error")
+            } else {
+                let vc = self.storyboard?.instantiateViewController(withIdentifier: "Profiles")
+                self.present(vc!, animated: true, completion: nil)
+            }
+        })
+    }
+    
+    
+    @IBAction func CreateAccount(_ sender: Any) {
+        FIRAuth.auth()?.createUser(withEmail: EmailField.text!, password: PassField.text!, completion: { (user, error) in
+            if error != nil {
+                print("Create Error")
+            } else {
+                print("User is created")
+                self.Login()
+            }
+        })
+    }
 
 }
 
